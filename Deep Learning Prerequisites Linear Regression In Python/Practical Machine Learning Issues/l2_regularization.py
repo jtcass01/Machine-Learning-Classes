f3448e3d308d5943ab84f506a761718cb302db46
Jacob Taylor Cassady <jtcass01@louisville.edu>
2017-04-13 09:54:35 -0500

    Create l2_regularization.py

---
+++ Deep Learning Prerequisites Linear Regression In Python/Practical Machine Learning Issues/l2_regularization.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 50
+
+X = np.linspace(0,10,N)
+Y = 0.5*X + np.random.randn(N)
+
+Y[-1] += 30
+Y[-1] += 30
+
+plt.scatter(X,Y)
+plt.show()
+
+# solve for the best weights
+#add the bias term
+X = np.vstack([np.ones(N), X]).T
+
+#calculate the maximum likelihood solution
+w_ml = np.linalg.solve(X.T.dot(X), X.T.dot(Y))
+Yhat_ml = X.dot(w_ml)
+plt.scatter(X[:,1], Y)
+plt.scatter(X[:,1], Yhat_ml)
+plt.show()
+
+#set the l2 penality
+l2 = 1000.0 #THIS IS AN ARBITARY NUMBER THAT COMES WITH TRIAL AND ERROR
+w_map = np.linalg.solve(l2*np.eye(2) + X.T.dot(X), X.T.dot(Y))
+Yhat_map = X.dot(w_map)
+plt.scatter(X[:,1], Y)
+plt.plot(X[:,1], Yhat_ml, label='maximum likelihood')
+plt.plot(X[:,1], Yhat_map, label = 'map')
+plt.legend()
+plt.show()
